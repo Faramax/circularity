476c670a031687afc798038a5d456e32f86b23c2
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2023-12-29 10:44:52 +0100

    circular::less was finished and tested

--- aligned.hpp
+++ aligned.hpp
@@ -2,20 +2,42 @@
 #include <type_traits>
 #include <cmath>
 
-namespace circ {
+namespace circular {
+
+   /// Less means minimum distance on circular numbers,
+   /// For instance less(250, 5) is true on UINT8_T because distance 250->5 is less than 5->250
    template <typename T>
    bool less(T const& l, T const& r)
    {
       static_assert(std::is_integral<T>::value, "Valid only for integral types");
-      auto const sub = typename std::make_signed<T>::type(l - r);
-      return sub < 0;
+      using signedT = typename std::make_signed<T>::type;
+      auto const sub = signedT(l - r);
+      return (sub != std::numeric_limits<signedT>::min()) && (sub < 0);
    }
 
+   /// Here we assume that the circle has begin and calculate distance in one direction from begin to l and r.
+   /// For instance less(250, 5, 0) is false on UINT8_T because distance 0->250 is more than 0->5
+   template <typename T>
+   bool less(T const& l, T const& r, T const& begin)
+   {
+      return false;
+   }
+
+   //! Direction independent distance between two points on the circle
+   //! The result is in the range [0, std::numeric_limits<T>::max()/2].
    template <typename T>
    T distance(T const& l, T const& r)
    {
       static_assert(std::is_integral<T>::value, "Valid only for the integral types");
       return std::abs(l - r);
+   }
+
+   //! Direction dependent distance strictly from point l to point r. We suppose that l always before r.
+   //! The result is in the range [0, std::numeric_limits<T>::max()].
+   template <typename T>
+   T track(T const& l, T const& r)
+   {
+      return T{};
    }
 
 } // circ
--- aligned_autotest.cpp
+++ aligned_autotest.cpp
@@ -5,7 +5,7 @@
 
 BOOST_AUTO_TEST_CASE( less_test )
 {
-   using namespace circ;
+   using namespace circular;
    BOOST_CHECK(less(0, 5));
    BOOST_CHECK(!less(5, 0));
    BOOST_CHECK(less(-5, 0));
@@ -14,30 +14,35 @@ BOOST_AUTO_TEST_CASE( less_test )
    BOOST_CHECK(!less(5, 5));
    BOOST_CHECK(!less(-5, -5));
 
-   BOOST_CHECK(less<ptrdiff_t>(-6, std::numeric_limits<ptrdiff_t>::max() - 5));
-   BOOST_CHECK(less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::max() - 5, -6));
+   BOOST_CHECK(!less<ptrdiff_t>(5, std::numeric_limits<ptrdiff_t>::min() + 5));
+   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() + 5, 5));
 
-   BOOST_CHECK(!less<ptrdiff_t>(-5, std::numeric_limits<ptrdiff_t>::max() - 6));
-   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::max() - 5, -5));
-   BOOST_CHECK(less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::max() - 4, -5));
+   BOOST_CHECK( less<ptrdiff_t>(5, std::numeric_limits<ptrdiff_t>::min() + 4));
+   BOOST_CHECK(!less<ptrdiff_t>(4, std::numeric_limits<ptrdiff_t>::min() + 5));
+   BOOST_CHECK( less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() + 5, 4));
+   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() + 4, 5));
 
+   BOOST_CHECK(!less<ptrdiff_t>(-5, std::numeric_limits<ptrdiff_t>::min() - 5));
+   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() - 5, -5));
 
-   BOOST_CHECK(less<ptrdiff_t>(5, std::numeric_limits<ptrdiff_t>::max() + 5));
-   BOOST_CHECK(!less<ptrdiff_t>(4, std::numeric_limits<ptrdiff_t>::max() + 5));
-   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::max() + 5, 5));
-   BOOST_CHECK(less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::max() + 6, 5));
+   BOOST_CHECK(!less<ptrdiff_t>(-6, std::numeric_limits<ptrdiff_t>::min() - 5));
+   BOOST_CHECK(less<ptrdiff_t>(-5, std::numeric_limits<ptrdiff_t>::min() - 6));
+   BOOST_CHECK(!less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() - 6, -5));
+   BOOST_CHECK(less<ptrdiff_t>(std::numeric_limits<ptrdiff_t>::min() - 5, -6));
 
-   BOOST_CHECK(less<uint8_t>(0x0,   0xEF));
-   BOOST_CHECK(less<uint8_t>(0xEF,  0xF0));
-   BOOST_CHECK(less<uint8_t>(0xF0,  0x0));
-   BOOST_CHECK(less<uint8_t>(0xF0,  0xFF));
-   BOOST_CHECK(less<uint8_t>(0xFF,  0x7F));
-   BOOST_CHECK(less<uint8_t>(0x80,  0xFF));
+   BOOST_CHECK( less<uint8_t>(0xEF, 0x0));
+   BOOST_CHECK( less<uint8_t>(0x7F, 0xEF));
+   BOOST_CHECK( less<uint8_t>(0x0,  0x7F));
+   BOOST_CHECK(!less<uint8_t>(0x0,  0x80));
+   BOOST_CHECK(!less<uint8_t>(0x80, 0x0));
+   BOOST_CHECK(!less<uint8_t>(0x7F, 0xFF));
+   BOOST_CHECK(!less<uint8_t>(0xFF, 0x7F));
+   BOOST_CHECK( less<uint8_t>(0x80, 0xFF));
 }
 
 BOOST_AUTO_TEST_CASE( distance_test )
 {
-   using namespace circ;
+   using namespace circular;
    BOOST_CHECK_EQUAL(distance(-5, 5), 10);
    BOOST_CHECK_EQUAL(distance(5, -5), 10);
    BOOST_CHECK_EQUAL(distance(5, 5), 0);
